af75685313b002088d417efe771b67403f6fb926
Roman Schmocker <roman.schmocker@ergon.ch>
2025-07-15 17:03:05 +0200

    Task 3: Grid for Laptop size

--- src/components/MainStoryGrid/MainStoryGrid.jsx
+++ src/components/MainStoryGrid/MainStoryGrid.jsx
@@ -63,6 +63,14 @@ const Wrapper = styled.div`
     grid-template-columns: 2fr 1fr;
   }
   
+  @media ${QUERIES.laptopAndUp}{
+      grid-template-areas:
+        'main-story secondary-stories opinion-stories'
+        'main-story advertisement advertisement';
+      grid-template-columns: 4fr 3fr 2fr;
+
+      gap: 16px;
+  }
 `;
 
 const MainStorySection = styled.section`
@@ -72,6 +80,11 @@ const MainStorySection = styled.section`
     padding-right: 24px;
     border-right: 1px solid var(--color-gray-300);
   }
+
+  @media ${QUERIES.laptopAndUp} {
+      padding-right: 16px;
+      border-right: 1px solid var(--color-gray-300);
+  }
 `;
 
 const SecondaryStorySection = styled.section`
@@ -80,19 +93,21 @@ const SecondaryStorySection = styled.section`
 
 const OpinionSection = styled.section`
   grid-area: opinion-stories;
+
+    @media ${QUERIES.laptopAndUp} {
+        padding-left: 16px;
+        border-left: 1px solid var(--color-gray-300);
+    }
 `;
 
 const StoryList = styled.div`
   display: flex;
   flex-direction: column;
   
-  > * {
-    padding-top: 16px;
-    padding-bottom: 16px;
-  }
-  
   > *:not(:last-child) {
+    padding-bottom: 16px;
     border-bottom: 1px solid var(--color-gray-300);
+    margin-bottom: 16px;
   }
   
   ${OpinionSection} & {
@@ -103,11 +118,17 @@ const StoryList = styled.div`
         border-bottom: none;
       }
     }
+
   }
 `;
 
 const AdvertisementSection = styled.section`
   grid-area: advertisement;
+    
+    @media ${QUERIES.laptopAndUp} {
+        padding-top: 16px;
+        border-top: 1px solid var(--color-gray-300);
+    }
 `;
 
 export default MainStoryGrid;
